578d6fc05c35802f984bdbb664e7b7853392f8cc
devindolence <devindolence@gmail.com>
2023-12-11 20:57:11 +0900

    Feat: start project
    - List to Excel
    - todo: do refactor.
    - todo: divide header from T Dto, and data from List
    
    Signed-off-by: devindolence <devindolence@gmail.com>

--- src/main/java/reference/Main.java
+++ src/main/java/reference/Main.java
@@ -1,8 +1,7 @@
 package reference;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.json.JSONObject;
+import org.json.JSONArray;
 import reference.dto.TestUser;
 import reference.dto.TestUser2;
 import reference.file.DtoToExcelConverter;
@@ -10,25 +9,34 @@ import reference.file.DtoToExcelConverter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        TestUser testUser = new TestUser("test1", "1");
-        TestUser testUser2 = new TestUser("test2", "2");
-        TestUser2 twoHeaderDto = new TestUser2(testUser, testUser2);
+        List<TestUser> testUsers = new ArrayList<>();
+        List<TestUser2> testUser2List = new ArrayList<>();
 
-        JSONObject oneHeader = DtoToExcelConverter.toJsonObject(testUser);
-        JSONObject twoHeader = DtoToExcelConverter.toJsonObject(twoHeaderDto);
+        for (int i = 0; i < 100; i++) {
+            TestUser testUser = new TestUser("test : " + i, String.valueOf(i));
+            TestUser testUser2 = new TestUser("test2 : " + i, String.valueOf(i));
+            testUsers.add(testUser);
+            testUsers.add(testUser2);
+
+            TestUser2 twoHeaderDto = new TestUser2(testUser, testUser2);
+            testUser2List.add(twoHeaderDto);
+        }
+
+        JSONArray oneHeader = DtoToExcelConverter.toJsonArray(testUsers);
+        JSONArray twoHeader = DtoToExcelConverter.toJsonArray(testUser2List);
 
         // Create a new Excel workbook
-
-
         // Create a new sheet in the workbook
         DtoToExcelConverter dtoToExcelConverter = new DtoToExcelConverter();
 
-        Workbook workbook = dtoToExcelConverter.toExcel(oneHeader);
-        Workbook workbook2 = dtoToExcelConverter.toExcel(twoHeader);
+        Workbook workbook = dtoToExcelConverter.toExcel(oneHeader, testUsers.size());
+        Workbook workbook2 = dtoToExcelConverter.toExcel(twoHeader, testUser2List.size());
 
 
         // Write to an Excel file
--- src/main/java/reference/file/DtoToExcelConverter.java
+++ src/main/java/reference/file/DtoToExcelConverter.java
@@ -7,8 +7,10 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.Collection;
 import java.util.Iterator;
 
 public class DtoToExcelConverter {
@@ -26,6 +28,19 @@ public class DtoToExcelConverter {
         return new JSONObject(jsonString);
     }
 
+    public static <T extends ExcelDto> JSONArray toJsonArray(Collection<T> excelDto) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString;
+
+        try {
+            jsonString = objectMapper.writeValueAsString(excelDto);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new JSONArray(jsonString);
+    }
+
     public Workbook toExcel(
             JSONObject jsonObject
     ) {
@@ -33,38 +48,46 @@ public class DtoToExcelConverter {
         Sheet sheet = workbook.createSheet("Sheet1");
 
         // Create the headers
-        Row headerRow = sheet.createRow(0);
-        Row subheaderRow = sheet.createRow(1);
-        Iterator<String> keys = jsonObject.keys();
-        int columnCount = 0;
-        while (keys.hasNext()) {
-            String key = keys.next();
-            Cell headerCell = headerRow.createCell(columnCount);
-            headerCell.setCellValue(key);
-            if (jsonObject.get(key) instanceof JSONObject) {
-                JSONObject subObject = jsonObject.getJSONObject(key);
-                Iterator<String> subKeys = subObject.keys();
-                int subColumnCount = columnCount;
-                while (subKeys.hasNext()) {
-                    String subKey = subKeys.next();
-                    Cell subheaderCell = subheaderRow.createCell(subColumnCount);
-                    subheaderCell.setCellValue(subKey);
-                    subColumnCount++;
-                }
-            }
-            columnCount++;
-        }
+        createHeader(jsonObject, sheet);
 
         // Add data to the cells
+        addCellData(jsonObject, sheet);
+
+        return workbook;
+    }
+
+    // list to excel
+    public Workbook toExcel(
+            JSONArray jsonArray,
+            int size
+    ) {
+        Workbook workbook = new HSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Sheet1");
+
+        // Create the headers
+        createHeader(jsonArray, sheet);
+
+        // Add data to the cells
+        addCellData(jsonArray, sheet, size);
+
+        return workbook;
+    }
+
+    // object to cell
+    private void addCellData(JSONObject jsonObject, Sheet sheet) {
+        int columnCount;
+        Iterator<String> keys;
         Row dataRow = sheet.createRow(2);
         keys = jsonObject.keys();
         columnCount = 0;
+        int subColumnCount = 0;
         while (keys.hasNext()) {
             String key = keys.next();
+
+            // if exists sub header
             if (jsonObject.get(key) instanceof JSONObject) {
                 JSONObject subObject = jsonObject.getJSONObject(key);
                 Iterator<String> subKeys = subObject.keys();
-                int subColumnCount = columnCount;
                 while (subKeys.hasNext()) {
                     String subKey = subKeys.next();
                     Cell dataCell = dataRow.createCell(subColumnCount);
@@ -77,7 +100,98 @@ public class DtoToExcelConverter {
             }
             columnCount++;
         }
+    }
 
-        return workbook;
+    // todo refactor
+    private void addCellData(JSONArray jsonArray, Sheet sheet, int size) {
+        int columnCount;
+        for (int i = 0 ; i < size ; i++) {
+            Iterator<String> keys;
+            Row dataRow = sheet.createRow(2+i);
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            keys = jsonObject.keys();
+            columnCount = 0;
+            int subColumnCount = 0;
+            while (keys.hasNext()) {
+                String key = keys.next();
+
+                // if exists sub header
+                if (jsonObject.get(key) instanceof JSONObject) {
+                    JSONObject subObject = jsonObject.getJSONObject(key);
+                    Iterator<String> subKeys = subObject.keys();
+                    while (subKeys.hasNext()) {
+                        String subKey = subKeys.next();
+                        Cell dataCell = dataRow.createCell(subColumnCount);
+                        dataCell.setCellValue(subObject.getString(subKey));
+                        subColumnCount++;
+                    }
+                } else {
+                    Cell dataCell = dataRow.createCell(columnCount);
+                    dataCell.setCellValue(jsonObject.getString(key));
+                }
+                columnCount++;
+            }
+        }
+    }
+
+    // JSONObject > T Property to Sheet Header Column. if T Object has group header, rows setting.
+    private void createHeader(JSONObject jsonObject, Sheet sheet) {
+        Row headerRow = sheet.createRow(0);
+        Row subheaderRow = sheet.createRow(1);
+        Iterator<String> keys = jsonObject.keys();
+        int columnCount = 0;
+        int subColumnCount = 0;
+        while (keys.hasNext()) {
+            String key = keys.next();
+            Cell headerCell = headerRow.createCell(columnCount);
+            headerCell.setCellValue(key);
+
+            // todo refactor
+            // if exists sub header
+            if (jsonObject.get(key) instanceof JSONObject) {
+                JSONObject subObject = jsonObject.getJSONObject(key);
+                Iterator<String> subKeys = subObject.keys();
+                while (subKeys.hasNext()) {
+                    String subKey = subKeys.next();
+                    Cell subheaderCell = subheaderRow.createCell(subColumnCount);
+                    subheaderCell.setCellValue(subKey);
+                    subColumnCount++;
+                    columnCount++;
+                }
+                columnCount--;
+            }
+            columnCount++;
+        }
+    }
+
+    // todo refactor
+    private void createHeader(JSONArray jsonArray, Sheet sheet) {
+        Row headerRow = sheet.createRow(0);
+        Row subheaderRow = sheet.createRow(1);
+        JSONObject jsonObject = jsonArray.getJSONObject(0);
+        Iterator<String> keys = jsonObject.keys();
+        int columnCount = 0;
+        int subColumnCount = 0;
+        while (keys.hasNext()) {
+            String key = keys.next();
+            Cell headerCell = headerRow.createCell(columnCount);
+            headerCell.setCellValue(key);
+
+            // todo refactor
+            // if exists sub header
+            if (jsonObject.get(key) instanceof JSONObject) {
+                JSONObject subObject = jsonObject.getJSONObject(key);
+                Iterator<String> subKeys = subObject.keys();
+                while (subKeys.hasNext()) {
+                    String subKey = subKeys.next();
+                    Cell subheaderCell = subheaderRow.createCell(subColumnCount);
+                    subheaderCell.setCellValue(subKey);
+                    subColumnCount++;
+                    columnCount++;
+                }
+                columnCount--;
+            }
+            columnCount++;
+        }
     }
 }
